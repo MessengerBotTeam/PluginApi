e2c9993172e27e11f940df48e8139e57abe77169
naijun0403 <naijun0403@gmail.com>
2025-08-22 11:15:33 +0900

    feat: add Maven publishing configuration to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.2.0"
+    `maven-publish`
 }
 
 group = "com.xfl.msgbot.plugin.api"
@@ -18,4 +19,23 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/MessengerBotTeam/PluginAPI")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
